03748003de629cc7e2f57bb16f5450d5143ff750
CallMeAl3x <Nakashi.iop@gmail.comgit config --global user.name CallMeAl3xgit commit -m First>
2023-12-13 10:23:32 +0100

    image to quizz

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,42 +7,42 @@ const db = [
     Intitulé: 'En 2023, le moyen le plus utilisé pour rencontrer des personnes est internet.',
     Réponse: 'Vrai',
     RéponseDéveloppé: "Vrai, en effet en effet d'après une étude faite aux Etat-Unis on compte 40% des gens se sont rencontré via internet en 2020.",
-    Image: './img/richard.jpg',
+    Image: "../img/representation.jpeg",
     Source: 'Disintermediating your friends: How Online Dating in the United States displaces other ways of meeting" Michael Rosenfeld & Sonia Hausen, Stanford University, Reuben J. Thomas, University of New Mexico Données provenant des enquêtes « How Couples Meet and Stay Together », 2009 et 2017',
   },
   {
     Intitulé: 'Plus d\'une personne sur deux avoue utiliser des filtres ou des retouches excessives sur leurs photos de profil.',
     Réponse: 'Vrai',
     RéponseDéveloppé: "Vrai, en effet plus de 60% des utilisateurs admettent utiliser des filtres ou des retouches pour améliorer leur apparence sur les photos de leurs profils.",
-    Image: './img/richard.jpg',
+    Image: '../img/representation.jpeg',
     Source: '',
   },
   {
     Intitulé: 'Les algorithmes des sites de rencontre sont conçus pour garantir des correspondances précises et durables entre les utilisateurs.',
     Réponse: 'Faux',
     RéponseDéveloppé: "Les algorithmes des sites de rencontre peuvent ne pas toujours mener à des correspondances précises ou durables. Ils sont souvent basés sur des critères superficiels.",
-    Image: './img/richard.jpg',
+    Image: '../img/representation.jpeg',
     Source: '',
   },
   {
     Intitulé: 'Les utilisateurs de sites de rencontre passent en moyenne moins de 30 minutes par jour',
     Réponse: 'Faux',
     RéponseDéveloppé: "Faux, en effet les utilisateurs de sites de rencontre passent en moyenne 35 minutes par jour",
-    Image: './img/richard.jpg',
+    Image: '../img/representation.jpeg',
     Source: '',
   },
   {
     Intitulé: 'Les utilisateurs ouvrent en moyenne l\'application Tinder plus de 5 fois par jour ?',
     Réponse: 'Vrai',
     RéponseDéveloppé: "Vrai, en effet les utilisateurs de l'application Tinder l'ouvrent en moyenne 11 fois par jour.",
-    Image: './img/richard.jpg',
+    Image: '../img/representation.jpeg',
     Source: '',
   },
   {
     Intitulé: 'La moitié des personnes âgées de moins de 30 ans ont déjà utilisé une application de rencontre.',
     Réponse: 'Vrai',
     RéponseDéveloppé: "",
-    Image: './img/richard.jpg',
+    Image: '../img/representation.jpeg',
     Source: '',
   },
 ]
@@ -121,6 +121,8 @@ const Main = () => {
                 <img src={sources} alt='sources' className='w-6 h-6 mr-4 mt-2' />
               </div>
               <h3 className='mt-2 text-center text-black'>{Question.Intitulé}</h3>
+              <img src="/img/representation.jpeg" className='h-52 select-none' draggable="false" 
+              alt="" />
             </div>
           </TinderCard>
         ))}
